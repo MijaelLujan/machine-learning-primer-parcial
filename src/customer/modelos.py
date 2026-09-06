9157ff123190edd3bf1e7b6b1791541cb766735d
zer4c <pabloazurduy40@gmail.com>
2026-09-05 22:43:40 -0400

    refactor: path files

---
+++ src/customer/modelos.py
@@ -0,0 +1,11 @@
+"""
+Módulo de modelos
+
+Acá va el código relacionado a:
+División de datos en entrenamiento y prueba
+Implementación del modelo (regresión o árbol de decisión, según el área asignada)
+
+No mezclar con las funciones de limpieza
+"""
+
+# implementar una vez definida la estrategia de train/test split.
